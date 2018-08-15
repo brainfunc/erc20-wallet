980e640c9916b4f3c7ba2b76580c2a6ef6051fa8
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-08-16 01:18:33 +0530

    Wallet complete

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ class App extends Component {
     this.state = {
       address: 0x00,
       balance: 0,
-      tokens: 0
+      tokens: 0,
+      tokensToSend: 0,
+      addressToSend: 0x00
     }
 
     this.setWalletAddress = this.setWalletAddress.bind(this);
     this.setEtherBalance = this.setEtherBalance.bind(this);
     this.setBFTTokenBalance = this.setBFTTokenBalance.bind(this);
+
     this.refreshState = this.refreshState.bind(this);
+
+    this.handleSendTokenChange = this.handleSendTokenChange.bind(this);
+    this.handleSendAddressChange = this.handleSendAddressChange.bind(this);
+
+    this.sendTokens = this.sendTokens.bind(this);
   }
 
   setWalletAddress() {
@@ -51,6 +59,33 @@ class App extends Component {
     })
   }
 
+  handleSendTokenChange(e) {
+    this.setState({
+      tokensToSend: e.target.value
+    })
+  }
+
+  handleSendAddressChange(e) {
+    this.setState({
+      addressToSend: e.target.value
+    })
+  }
+
+  sendTokens() {
+    const contract = window.web3.eth.contract(config.contractAbi)
+    .at(config.contractAddress, (err, ctr) => {});
+    if(!window.web3.eth.defaultAccount) { return }
+    contract.transfer(this.state.addressToSend,
+      window.web3.toWei(this.state.tokensToSend, 'ether'),
+      (err,res) => {
+      if(err) {
+        console.log("Error:", err);
+        return;
+      }
+      console.log("Transaction Hash: ", res);
+    });
+  }
+
   componentDidMount() {
     this.refreshState()
   }
@@ -71,6 +106,18 @@ class App extends Component {
           <b> BFT Token Balance: </b> { this.state.tokens } <br/>
         </div>
         <button onClick= { this.refreshState }> Refresh </button>
+        <h1> Send BrainFunc Tokens </h1>
+
+        <h4>Address To Send</h4>
+        <input
+        onChange={this.handleSendAddressChange}
+        value={this.addressToSend} />
+
+        <h4> Amount to Send </h4>
+        <input
+        onChange={this.handleSendTokenChange}
+        value={this.tokensToSend} /> <br/>
+        <button onClick={this.sendTokens}> Send </button>
       </div>
     );
   }
